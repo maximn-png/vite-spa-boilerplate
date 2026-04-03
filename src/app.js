3da37af237d483233ddccec6acc13278bea3bf45
Maxim Naftaliyv <maxim.n@easybim.co.il>
2026-04-03 14:57:37 +0300

    Add Vite SPA boilerplate scaffold

---
+++ src/app.js
@@ -0,0 +1,8 @@
+export function setupApp(element) {
+  element.innerHTML = `
+    <div class="container">
+      <h1>Vite SPA Boilerplate</h1>
+      <p>Edit <code>src/app.js</code> to get started.</p>
+    </div>
+  `
+}
